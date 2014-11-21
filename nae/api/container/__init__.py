28522a8f54110d747d8859948611ea2eb648e44a
niuminguo <minguon@jumei.com>
2014-11-21 18:39:47 -0500

    modified:mapper.connect->mapper.resource

--- nae/api/container/__init__.py
+++ nae/api/container/__init__.py
@@ -11,59 +11,10 @@ class APIRouter(wsgi.Router):
 	super(APIRouter,self).__init__(self.mapper)
 
     def setup_routes(self):
-        self.mapper.connect('/containers',
-        		controller=self.resource,
-        		action='index',
-        		conditions={'method':['GET']},
-        )
-        self.mapper.connect('/containers/{container_id}',
-        		controller=self.resource,
-        		action='delete',
-        		conditions={'method':['DELETE']},
-        )
-        
-        self.mapper.connect('/containers/{container_id}',
-        		controller=self.resource,
-        		action='show',
-        		condition={'method':['GET']},
-        )
-        
-        self.mapper.connect('/containers/{container_id}',
-        		controller=self.resource,
-        		action='inspect',
-        		conditions={'method':['GET']},
-        )
-
-        self.mapper.connect('/containers/{container_id}/stop',
-        		controller=self.resource,
-        		action='stop',
-        		conditions={'method':['POST']},
-        )
-        self.mapper.connect('/containers/{container_id}/start',
-        		controller=self.resource,
-        		action='start',
-        		conditions={'method':['POST']},
-        )
-        self.mapper.connect('/containers/{container_id}/reboot',
-        		controller=self.resource,
-        		action='reboot',
-        		conditions={'method':['POST']},
-        )
-        self.mapper.connect('/containers/{container_id}/commit',
-        		controller=self.resource,
-        		action='commit',
-        		conditions={'method':['POST']},
-        )
-        
-        self.mapper.connect('/containers/{container_id}/destroy',
-        		controller=self.resource,
-        		action='destroy',
-        		conditions={'method':['POST']},
-        )
-
-        
-        self.mapper.connect('/containers',
-        		controller=self.resource,
-        		action='create',
-        		conditions={'method':['POST']},
-        )
+	self.mapper.resource('container','containers',
+			     controller=self.resource,
+			     member={'start':'POST',
+				     'stop':'POST',
+                                     'reboot':'POST',
+				     'commit':'POST',
+                                     'destroy':'POST'})

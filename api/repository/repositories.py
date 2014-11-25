e71c7c0292e09ed4a8d5776f77f91a00fa48c0b6
nmg <nmg1986@126.com>
2014-11-25 17:19:47 +0800

    beautiful code

--- api/repository/repositories.py
+++ api/repository/repositories.py
@@ -16,6 +16,7 @@ class RepoController(object):
                 'Created':item[3],
                 }
             repos.append(repo)
+
         return repos 
 
     def create(self,request):
@@ -25,16 +26,15 @@ class RepoController(object):
         self.db_api.add_repo(
                 project_id = project_id,
                 repo_path= repo_path,
-                created = created,
-        )
-        ret = {"status":200}
-        return ret
+                created = created)
+
+        return {"status":200}
 
     def delete(self,request):
         repo_id=request.environ['wsgiorg.routing_args'][1]['id']
         self.db_api.delete_repo(hg_id)
-        result='{"status":200}'
-        return result
+
+        return {"status":200}
 
 def create_resource():
     return wsgi.Resource(RepoController())

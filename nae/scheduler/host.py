a25c3017bfa522165ae48da62977356fc1dc69e4
minguon <minguon@jumei.com>
2014-12-07 18:15:39 +0800

    changed: add scheduler host class

---
+++ nae/scheduler/host.py
@@ -0,0 +1,18 @@
+class Host(object):
+    def __init__(self,ram):
+        self._ram = ram 
+	self._address =address
+        sefl._port = port
+
+    @property
+    def weight(self):
+        return self._ram
+
+
+    @property
+    def address(self):
+        return self._address
+
+    @property
+    def port(self):
+        return self._port

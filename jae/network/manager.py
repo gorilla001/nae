c6af2ef3acb0daa104063c0a0d0ef9299a6806e1
niuminguo <minguon@jumei.com>
2014-12-15 03:00:53 -0500

    modified: rename nae to jae

---
+++ jae/network/manager.py
@@ -0,0 +1,39 @@
+import netaddr
+from netaddr import IPRange, IPAddress
+
+from jae.common import cfg
+from jae import db
+from jae.base import Base
+from jae.common import exception
+
+CONF=cfg.CONF
+
+FIXED_RANGE=None
+
+class NetworkManager(Base):
+    def get_fixed_ip(self):
+	global FIXED_RANGE
+	if not FIXED_RANGE:
+	    FIXED_RANGE = self.get_fixed_range() 
+	
+	query = self.db.get_containers()
+	if not query:
+	    for item in query:
+		if IPAddress(item.fixed_ip) in FIXED_RANGE:
+		    FIXED_RANGE.remove(IPAddress(item.fixed_ip))
+
+	try:
+	    return str(FIXED_RANGE[0])	
+	except IndexError:
+	    raise NoValidIPAddress(msg='ip resource used up')
+
+    def get_fixed_range(self):
+	ip_resource_pool = CONF.ip_resource_pool 	
+	if not ip_resource_pool:
+	    raise NoValidIPAddress(msg='ip resource pool is None')
+	
+	start,_,end=ip_resource_pool.rpartition("-")
+
+	ip_range = IPRange(start,end)
+
+	return list(ip_range)

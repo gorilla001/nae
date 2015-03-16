bbe017372a3504f805e0bd2e686a7e7e34f64cb7
niuminguo <nmg1986@126.com>
2015-03-17 02:58:56 +0800

    use jinja2 for template render

--- nae/web/render.py
+++ nae/web/render.py
@@ -1,12 +1,17 @@
 from nae.common import cfg
 import os
-import tempita
+#import tempita
+from jinja2 import Environment, FileSystemLoader
 import webob.response
 
 CONF=cfg.CONF
 
-def render(template, **vars):
-    template_file = os.path.join(os.path.dirname(__file__),"template",template)
-    template = tempita.HTMLTemplate.from_filename(template_file)
-    response = template.substitute(vars) 
+def render(template_file, **vars):
+    template_dir = os.path.join(os.path.dirname(__file__), "template")
+    #template_file = os.path.join(template_dir,template)
+    env = Environment(loader = FileSystemLoader(template_dir))
+    template = env.get_template(template_file)
+    response = template.render(vars)
+    #template = tempita.HTMLTemplate.from_filename(template_file)
+    #response = template.substitute(vars) 
     return webob.response.Response(body=response,content_type='text/html')
